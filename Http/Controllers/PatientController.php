d99876b50e8cc58d14438b48de8ce4f5cd8c8f14
Samuel Dervis <samueldervis@gmail.com>
2017-10-05 11:53:39 +0300

    Changes for Ena

--- Http/Controllers/PatientController.php
+++ Http/Controllers/PatientController.php
@@ -3,39 +3,44 @@
 namespace Ignite\Reports\Http\Controllers;
 
 use Ignite\Core\Http\Controllers\AdminBaseController;
+use Ignite\Evaluation\Entities\DoctorNotes;
 use Ignite\Evaluation\Entities\Investigations;
 use Ignite\Evaluation\Entities\Prescriptions;
 use Ignite\Evaluation\Entities\Visit;
 Use Ignite\Settings\Entities\Clinics;
 use Ignite\Users\Entities\UserProfile;
 
-class PatientController extends AdminBaseController {
+class PatientController extends AdminBaseController
+{
 
     /**
      * Filter procedures
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function procedures() {
+    public function procedures()
+    {
         $this->data['investigations'] = Investigations::where('type', '<>', 'treatment')->get();
-        //Charts::procedureCharts($this->data['investigations']);
         return view('reports::patients.procedures', ['data' => $this->data]);
     }
 
-    public function treatment() {
+    public function treatment()
+    {
         $this->data['clinics'] = Clinics::all();
         $this->data['clinician'] = UserProfile::where('job_description', '=', 'Doctor')
-                ->orWhere('job_description', '=', 'Nurse')
-                ->get();
-        $this->data['investigations'] = Investigations::whereType('treatment')->get();
+            ->orWhere('job_description', '=', 'Nurse')
+            ->get();
+        $this->data['diagnoses'] = DoctorNotes::whereNotNull('diagnosis')->get();
         return view('reports::patients.treatment', ['data' => $this->data]);
     }
 
-    public function medication() {
+    public function medication()
+    {
         $this->data['medication'] = Prescriptions::all();
         return view('reports::patients.medicine', ['data' => $this->data]);
     }
 
-    public function visits() {
+    public function visits()
+    {
         $this->data['visits'] = Visit::all();
         //Charts::visitCharts($this->data['visits']);
         return view('reports::patients.visits', ['data' => $this->data]);
--- Resources/views/patients/treatment.blade.php
+++ Resources/views/patients/treatment.blade.php
@@ -11,99 +11,80 @@ extract($data);
 @section('content_description','Analytics for patient treatement')
 
 @section('content')
-<div class="box box-info">
-    <div class="box-body">
-        <div class="row col-md-8">
-            <div class="col-md-2">
-                Clinic:
-            </div>
-            <div class="col-md-4">
-                <select class="form-control" id="clinic">
-                    @foreach($clinics as $item)
-                    <option value="{{$item->id}}">{{$item->name}}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-md-2">
-                Doctor:
-            </div>
-            <div class="col-md-4">
-                <select class="form-control" id="clinician">
-                    @foreach($clinician as $item)
-                    <option value="{{$item->user}}">{{$item->name}}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-        <table class="table table-striped">
-            <tbody id="result">
-                @foreach($investigations as $procedure)
-                <?php
-                // try {
-                // $procedure->visits->patients;
-                ?>
-                <tr>
-                    <td>{{$procedure->id}}</td>
-                    <td>{{$procedure->procedures->name}}</td>
-                    <td>{{$procedure->visits?$procedure->visits->patients->first_name:''}}</td>
-                    <td>{{smart_date_time($procedure->created_at)}}</td>
-                    <td>{{$procedure->visits?$procedure->visits->clinics->name:''}}</td>
-                    <td>{{$procedure->doctors?$procedure->doctors->profile->full_name:''}}</td>
-                    <td>{{$procedure->pesa}}</td>
-                    <td>{{$procedure->visits?$procedure->visits->payment_mode:''}}</td>
-                </tr>
-                <?php
-                //  } catch (\Exception $ex) {
-                //  }
-                ?>
+    <div class="box box-info">
+        <div class="box-body">
+            {{--<div class="row col-md-8">--}}
+            {{--<div class="col-md-2">--}}
+            {{--Doctor:--}}
+            {{--</div>--}}
+            {{--<div class="col-md-4">--}}
+            {{--<select class="form-control" id="clinician">--}}
+            {{--@foreach($clinician as $item)--}}
+            {{--<option value="{{$item->user}}">{{$item->name}}</option>--}}
+            {{--@endforeach--}}
+            {{--</select>--}}
+            {{--</div>--}}
+            {{--</div>--}}
+            <table class="table table-striped">
+                <tbody id="result">
+                @foreach($diagnoses as $item)
+                    <tr>
+                        <td>{{$loop->iteration}}</td>
+                        <td>{{$item->visits->created_at->format('d/m/Y')}}</td>
+                        <td>{{$item->visits->patients->id}}</td>
+                        <td>{{$item->visits->patients->full_name}}</td>
+                        <td>{{$item->visits->patients->age}}</td>
+                        <td>{{$item->visits->patients->sex}}</td>
+                        <td>{{$item->visits->patients->town}}</td>
+                        <td>{{$item->visit_type}}</td>
+                        <td>{{$item->diagnosis}}</td>
+                        <td>{{$item->doctor->profile->name??'-'}}</td>
+                    </tr>
                 @endforeach
-            </tbody>
-            <thead>
+                </tbody>
+                <thead>
                 <tr>
                     <th>#</th>
-                    <th>Procedure</th>
-                    <th>Patient</th>
-                    <th>Date / Time</th>
-                    <th>Clinic</th>
-                    <th>Clinician</th>
-                    <th>Revenue</th>
-                    <th>Mode</th>
+                    <th>Visit Date</th>
+                    <th>Patient ID</th>
+                    <th>Patient Name</th>
+                    <th>Age</th>
+                    <th>Gender</th>
+                    <th>Residence</th>
+                    <th>Visit Type</th>
+                    <th>Diagnosis</th>
+                    <th>Doctor</th>
                 </tr>
-            </thead>
-        </table>
+                </thead>
+            </table>
+        </div>
     </div>
-</div>
-<script type="text/javascript">
+    <script type="text/javascript">
+        $('#clinician').change(function () {
+            FetchDiagnoses('clinician', this.value);
+        });
 
-    $('#clinic').change(function () {
-        FetchDiagnoses('clinic', this.value);
-    });
+        function FetchDiagnoses(type, value) {
+            $(document).ready(function () {
+                $.ajax({
+                    type: 'get',
+                    url: "{{route('api.reports.diagnoses.clinic')}}",
+                    data: {'type': type, 'value': value},
+                    dataType: 'html',
+                    success: function (response) {
+                        $('#result').html(response);
+                    }
+                });
+            });
+        }
 
-    $('#clinician').change(function () {
-        FetchDiagnoses('clinician', this.value);
-    });
-
-    function FetchDiagnoses(type, value) {
-        $(document).ready(function () {
-            $.ajax({
-                type: 'get',
-                url: "{{route('api.reports.diagnoses.clinic')}}",
-                data: {'type': type, 'value': value},
-                dataType: 'html',
-                success: function (response) {
-                    $('#result').html(response);
-                }
+        $(function () {
+            $('table').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'excel', 'pdf', 'print'
+                ]
             });
         });
-    }
-
-    $(function () {
-        $('table').DataTable({
-            dom: 'Bfrtip',
-            buttons: [
-                'excel', 'pdf', 'print'
-            ]
-        });
-    });
-</script>
+    </script>
 @endsection
