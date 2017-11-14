01e9967532ae7213d1b0a11f8a9f51dd577f257a
Samuel Dervis <samueldervis@gmail.com>
2017-11-14 19:14:44 +0300

    Misc

--- helpers.php
+++ helpers.php
@@ -6,19 +6,24 @@ if (!function_exists('filter_description')) {
      * @param array $data
      * @return string A highly opinioneted filter dexcription for dates
      */
-    function filter_description(array $data = null) {
-        $text = "Showing records ";
+    function filter_description(array $data = null)
+    {
+        $text = 'Showing records ';
         if (empty($data['from']) && empty($data['to'])) {
-            $text = "Showing all records available";
+            $text = 'Showing all  records available';
         }
-        if (!empty($data['from']))
-            $text.=" from " . $data['from'];
-        if (!empty($data['to']))
-            $text.=" up to " . $data['to'];
+        if ($data['from'] == $data['to']) {
+            $text .= ' for ' . $data['from'];
+        } else {
+            if (!empty($data['from']))
+                $text .= ' from ' . $data['from'];
+            if (!empty($data['to']))
+                $text .= ' up to ' . $data['to'];
+        }
         if (!empty($data['mode']))
-            $text.=". Payment mode " . ucfirst($data['mode']);
+            $text .= '. Payment mode ' . ucfirst($data['mode']);
         if (!empty($data['department']))
-            $text.=". Department: " . ucfirst($data['department']);
+            $text .= '. Department: ' . ucfirst($data['department']);
         return $text;
     }
 
